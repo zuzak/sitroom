6c41d76983897858cb33530be9cfc368c6b00bfa
Douglas Gardner <douglas@chippy.ch>
2014-03-24 04:45:53 +0000

    pretty syntax on

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,6 +2,6 @@ console.log('loaded')
 module.exports = function (app) {
   app.get('/', function (req, res) {
     console.log('got!')
-    res.render('home')
+    res.render('home',{pretty:true})
   })
 }
